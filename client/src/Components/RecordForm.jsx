daf62d961317b69ae66f36ff554e2a3ba758e11e
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-04-18 00:12:34 +0000

    Received form from client to server, formatting sql for inserting into database.

--- client/src/Components/RecordForm.jsx
+++ client/src/Components/RecordForm.jsx
@@ -1,13 +1,47 @@
+import CurrencyInput from 'react-currency-input-field';
+import { useState } from 'react';
 
 export default function RecordForm() {
+  const [items, setItems] = useState(1);
+
+  function handleItems (e) {
+    const numberOfItems = e.target.value;
+    setItems(numberOfItems);
+  }
+
+  async function handleSubmit (e) {
+
+    try {
+      e.preventDefault();
+      console.log('the form', e.target[0].value)
+      const form = new FormData(e.target)
+      for (const pair of form.entries()) {
+        console.log(`${pair[0]}, ${pair[1]}`);
+      }
+      const response = await fetch('/getsome/fires/record', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Object.fromEntries(form.entries()))
+      });
+      if(!response.ok) throw new Error(`Server error: ${response.status}`)
+      const responseBody = await response.json();
+      console.log('Success form sent', responseBody);
+    } catch (err) {
+      console.error(err)
+    }
+
+  }
+
   return (
     <div>
       <h1>New Record</h1>
-    <form style={{backgroundColor: 'gray', color: 'black'}}>
+    <form style={{backgroundColor: 'gray', color: 'black'}} onSubmit={handleSubmit}>
 
     <div>
       <label htmlFor="inOut">Select Type</label>
-      <select name="inOut" id="inOut">
+      <select required name="inOut" id="inOut">
         <option value={true}>Money Out</option>
         <option value={false}>Money In</option>
       </select>
@@ -15,14 +49,59 @@ export default function RecordForm() {
 
     <div>
       <label htmlFor="source">Source:</label>
-      <input type="text" id="source" name="source" />
+      <input required type="text" id="source" name="source" />
     </div>
 
     <div>
       <label htmlFor="numberOfItems"># of Items</label>
-      <input type="number" name="numberOfIems" id="numberOfItems" min={1} max={20} />
+      <input required type="number" name="numberOfItems" defaultValue={1} id="numberOfItems" min={1} max={20} onChange={(e) => handleItems(e)} />
     </div>
+
+    <div>
+      <label htmlFor="total">Total $</label>
+      <CurrencyInput required id='total' name='total' placeholder='Enter total $' decimalsLimit={2} onValueChange={(value, name) => console.log(value, name)} />
+    </div>
+
+    <div>
+      <label htmlFor="date">Date</label>
+      <input type="date" name="date" id="date" onChange={(e) => console.log('the date', e.target.value)}/>
+    </div>
+
+    <div>
+      <button type='submit'>Submit</button>
+    </div>
+    <ItemForms numberOfItems={items}/>
     </form>
     </div>
+  )
+}
+
+function ItemForms({ numberOfItems }) {
+  const form = [];
+  for (let i = 0; i < numberOfItems; i++){
+    form.push(
+    <li key={i}>
+      <div>
+        <label htmlFor={`item${i}`}>{`Item #${i + 1}`}</label>
+        <input type="text" name={`item${i}`} id={`item${i}`} />
+      </div>
+
+      <div>
+          <label htmlFor={`itemCat${i}`}>Catergory</label>
+          <select name={`itemCat${i}`} id={`itemCat${i}`}>
+            <option value='food'>Food</option>
+            <option value='clothes'>Clothes</option>
+          </select>
+      </div>
+
+      <div>
+          <label htmlFor={`itemAmt${i}`}>Amount</label>
+          <CurrencyInput required id={`itemAmt${i}`} name={`itemAmt${i}`} placeholder='Amount $' decimalsLimit={2} />
+      </div>
+    </li>)
+  }
+
+  return (
+    <ul>{form}</ul>
   )
 }
--- server/server.js
+++ server/server.js
@@ -45,18 +45,29 @@ app.post('/getsome/fires', async (req, res, next) => {
  * Requires userId added to body at this point in time.
  */
 app.post('/getsome/fires/record', async (req, res, next) => {
-  console.log('new record');
-  const body = req.body;
+  const { inOut, source, numberOfItems, total, date } = req.body;
+  const dateArray = date.split('-');
+  const [year, month, day] = dateArray;
+  console.log('the form', req.body);
+  const items = createItemsList(req, numberOfItems);
+  console.log('The items!', items);
+  // const itemsSql = createItemsSql(numberOfItems, recordId);
   const sql = `
               insert into "records" ("userId", "month", "day", "year", "source", "inOut", "numberOfItems", "totalSpent")
               values ($1, $2, $3, $4, $5, $6, $7, $8)
               returning *;
               `;
-  const params = [1, body.month, body.day, body.year, body.source, body.inOut, body.numberOfItems, body.totalSpent];
+  const params = [1, month, day, year, source, inOut, numberOfItems, total];
   try {
     console.log('adding record as:', params);
     const data = await db.query(sql, params);
-    res.status(201).json(data.rows);
+    const { recordId } = data.rows[0];
+    const itemsSql = createItemsSql(numberOfItems, recordId);
+    console.log(data.rows);
+    console.log('The item sql', itemsSql);
+    console.log('the record:', recordId);
+    const response = { record: data.rows[0] };
+    res.status(201).json(response);
   } catch (err) {
     next(err);
   }
@@ -71,3 +82,33 @@ app.use(errorMiddleware);
 app.listen(process.env.PORT, () => {
   process.stdout.write(`\n\napp listening on port ${process.env.PORT}\n\n`);
 });
+
+function createItemsList(req, numberOfItems) {
+  const items = [];
+  for (let i = 0; i < numberOfItems; i++) {
+    const item = [req.body[`item${i}`], req.body[`itemCat${i}`], req.body[`itemAmt${i}`]
+    ];
+    items.push(item);
+  }
+  return items;
+}
+
+function createItemsSql(numberOfItems, recordId) {
+  const values = (numberOfItems) => {
+    let stringValues = '';
+    for (let i = 0; i < numberOfItems; i++) {
+      if ((i + 1) === Number(numberOfItems)) {
+        stringValues += `($${i + 1})`;
+      } else {
+        stringValues += `($${i + 1}),`;
+      }
+    }
+    return stringValues;
+  };
+  const sql = `
+              insert into "items" ("recordId", "category", "amount", "itemname")
+              values (${values(numberOfItems)})
+              returning *;
+              `;
+  return sql;
+}
