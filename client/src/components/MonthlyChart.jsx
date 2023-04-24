3d618e236a36139ef1adbec72b867471bae9af5e
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-04-24 17:05:58 +0000

    Added title to chart.

--- client/src/components/MonthlyChart.jsx
+++ client/src/components/MonthlyChart.jsx
@@ -10,17 +10,18 @@ import { useContext } from 'react';
 export function MonthlyChart({ chartData }) {
   const { isLargeScreen } = useContext(AppContext)
   return (
-    <div style={isLargeScreen ? { width: '75%', backgroundColor: 'white', display: 'flex', justifyContent: 'center' } : { width: '100%', backgroundColor: 'white' }} className='pe-3 pb-3 pt-3 my-5 rounded'>
+    <div style={isLargeScreen ? { width: '75%', backgroundColor: 'white'} : { width: '100%', backgroundColor: 'white' }} className='pe-3 pb-3 pt-3 my-5 rounded'>
+      <h3>Daily Totals</h3>
       <ResponsiveContainer aspect={2} width='100%'>
         <LineChart data={chartData}>
           <CartesianGrid stroke="#ccc" />
           <XAxis dataKey='day' minTickGap={30} />
           <YAxis type="number" domain={[0, 'dataMax + 50']} />
           <Legend />
-          <Line type="monotone" name='This Months Debits' dataKey='thisMonthDebit' stroke="#eb7134" strokeWidth={2} />
-          <Line type="monotone" name='This Months Credits' dataKey='thisMonthCredit' stroke="#37eb34" strokeWidth={2} />
-          <Line type="monotone" name='Last Months Debits' dataKey='lastMonthDebit' stroke="#3f4704" strokeWidth={2} />
-          <Line type="monotone" name='Last Months Credits' dataKey='lastMonthCredit' stroke="#3493eb" strokeWidth={2} />
+          <Line type="monotone" name='This Months Debits' dataKey='thisMonthDebit' stroke="#eb7134" strokeWidth={2} dot={false} />
+          <Line type="monotone" name='This Months Credits' dataKey='thisMonthCredit' stroke="#37eb34" strokeWidth={2} dot={false} />
+          <Line type="monotone" name='Last Months Debits' dataKey='lastMonthDebit' stroke="#3f4704" strokeWidth={2} dot={false} />
+          <Line type="monotone" name='Last Months Credits' dataKey='lastMonthCredit' stroke="#3493eb" strokeWidth={2} dot={false} />
         </LineChart>
       </ResponsiveContainer>
     </div>
