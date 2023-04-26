c2a654387fe19f34e99131b4f3d2f44858bebaa3
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-04-26 05:09:00 +0000

    Sign-up front end done, need to write fetch command.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import AppContext from "../components/AppContext"
 import { useContext, useState } from "react"
 import Modal from "react-bootstrap/Modal"
 import Button from "react-bootstrap/Button"
+import Form from 'react-bootstrap/Form'
 
 export default function Home() {
   const [showSignIn, setShowSignIn] = useState(false);
@@ -17,6 +18,7 @@ export default function Home() {
         <Button onClick={() => setShowSignUp(true)}>Sign Up</Button>
       </div>
       {showSignIn ? <SignInModal showSignIn={showSignIn} setShowSignIn={setShowSignIn} /> : ''}
+      {showSignUp ? <SignUpModal showSignUp={showSignUp} setShowSignUp={setShowSignUp} /> : ''}
     </div>
   )
 }
@@ -75,4 +77,75 @@ function SignInModal({ showSignIn, setShowSignIn }) {
         </form>
       </Modal>
   );
+}
+
+function SignUpModal({ showSignUp, setShowSignUp }) {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [verifyPassword, setVerifyPassword] = useState('')
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [email, setEmail] = useState('')
+  const [passwordMatch, setPasswordMatch] = useState()
+
+  function signUp(e) {
+    e.preventDefault()
+    console.log('password match', passwordMatch)
+    if (password !== verifyPassword) return setPasswordMatch(false)
+    setPasswordMatch(true)
+  }
+
+
+  return (
+    <Modal show={showSignUp} onHide={() => setShowSignUp(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Sign Up</Modal.Title>
+      </Modal.Header>
+      <form onSubmit={e => signUp(e)}>
+        <Modal.Body>
+          <div className='row g-3'>
+
+            <div className='col'>
+              <label className='form-label'>Username
+                <input value={username} onChange={e => setUsername(e.target.value)} required className='form-control' type='text' name='username' id='username' placeholder='Username' />
+              </label>
+              <label className='form-label'>Password
+                <Form.Control isValid={passwordMatch} value={password} onChange={e => setPassword(e.target.value)} required className='form-control' type='password' name='password' id='password' placeholder='Password' />
+                <Form.Control.Feedback type="valid">
+                  Passwords Match.
+                </Form.Control.Feedback>
+              </label>
+              <label className='form-label'>Verify Password
+                <Form.Control isInvalid={passwordMatch === false ? true : false} type="password" required value={verifyPassword} onChange={e => setVerifyPassword(e.target.value)} placeholder='Verify Password'/>
+                <Form.Control.Feedback type="invalid">
+                  Please choose a username.
+                </Form.Control.Feedback>
+              </label>
+            </div>
+
+            <div className='col'>
+              <label className='form-label'>First Name
+                <input value={firstname} onChange={e => setFirstname(e.target.value)} required className='form-control' type='text' name='firstname' id='firstname' placeholder='First Name' />
+              </label>
+              <label className='form-label'>Last Name
+                <input value={lastname} onChange={e => setLastname(e.target.value)} required className='form-control' type='text' name='lastname' id='lastname' placeholder='Last Name' />
+              </label>
+              <label className='form-label'>Email
+                <input  value={email} onChange={e => setEmail(e.target.value)} required className='form-control' type='email' name='email' id='email' placeholder='email@email.com' />
+              </label>
+            </div>
+
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" type="button" onClick={() => setShowSignUp(false)}>
+            Cancel
+          </Button>
+          <Button variant="primary" type="submit">
+            Sign Up
+          </Button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  )
 }
