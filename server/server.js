a46fb7d4f49c692a3a2d2aadaec90ff9b9d89921
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-05-16 22:10:42 +0000

    Testing build

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ const reactStaticDir = new URL('../client/build', import.meta.url).pathname;
 const uploadsStaticDir = new URL('public', import.meta.url).pathname;
 
 app.use(express.static(reactStaticDir));
+app.use(express.static('../client/build/'));
 // Static directory for file uploads server/public/
 app.use(express.static(uploadsStaticDir));
 app.use(express.json());
