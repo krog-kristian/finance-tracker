e9b60410220eb5d4001ebcb0dabea8b9af8336ee
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-05-16 18:54:23 +0000

    Removed some unesscary files and placeholder content.

--- database/schema.sql
+++ database/schema.sql
@@ -6,59 +6,6 @@ drop schema "public" cascade;
 
 create schema "public";
 
--- CREATE TABLE "public"."users" (
--- 	"firstname" TEXT NOT NULL,
--- 	"lastname" TEXT NOT NULL,
--- 	"createdat" timestamptz(6) not null default now(),
--- 	"lastlogin" timestamptz(6) not null default now(),
--- 	"password" TEXT NOT NULL,
--- 	"username" TEXT NOT NULL UNIQUE,
--- 	"email" TEXT NOT NULL UNIQUE,
--- 	"userId" serial NOT NULL,
--- 	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
--- ) WITH (
---   OIDS=FALSE
--- );
-
-
-
--- CREATE TABLE "public"."records" (
--- 	"userId" integer NOT NULL,
--- 	"month" integer NOT NULL,
--- 	"year" integer NOT NULL,
--- 	"day" integer NOT NULL,
--- 	"source" TEXT NOT NULL,
--- 	"isDebit" BOOLEAN NOT NULL,
--- 	"created" timestamptz(6) NOT NULL default now(),
--- 	"recordId" serial NOT NULL,
--- 	"numberOfItems" integer NOT NULL,
--- 	"totalSpent" DECIMAL NOT NULL,
--- 	CONSTRAINT "records_pk" PRIMARY KEY ("recordId")
--- ) WITH (
---   OIDS=FALSE
--- );
-
-
-
--- CREATE TABLE "public"."items" (
--- 	"recordId" integer NOT NULL,
--- 	"category" TEXT NOT NULL,
--- 	"amount" DECIMAL NOT NULL,
--- 	"itemname" TEXT NOT NULL,
--- 	"itemId" serial NOT NULL,
--- 	CONSTRAINT "items_pk" PRIMARY KEY ("itemId")
--- ) WITH (
---   OIDS=FALSE
--- );
-
-
-
-
--- ALTER TABLE "records" ADD CONSTRAINT "records_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
-
--- ALTER TABLE "items" ADD CONSTRAINT "items_fk0" FOREIGN KEY ("recordId") REFERENCES "records"("recordId");
-
-
 CREATE TABLE "public"."users" (
 	"firstName" TEXT NOT NULL,
 	"lastName" TEXT NOT NULL,
